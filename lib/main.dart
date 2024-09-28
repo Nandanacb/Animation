d35f840a2b98fb95396da6e3bd659e2eb4e3aa2f
Nandanacb <nandhnacb@gmail.com>
2024-09-28 13:15:11 +0530

    anime 28/9/24

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:animations/animatedcontainer.dart';
 import 'package:animations/fadetransition.dart';
 import 'package:animations/rotatetransition.dart';
 import 'package:animations/slidertransition.dart';
+import 'package:animations/tweentransition.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RotateTransitionDemo(),
+      home: TweenAnimationBuilderExample(),
     );
   }
 }
